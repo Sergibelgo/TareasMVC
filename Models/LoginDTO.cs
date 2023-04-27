d65f07c1f9c858564a0937ce696eedea382badc2
Sergibelgo <sergibelgo1@gmail.com>
2023-04-27 18:58:54 +0200

    Step 209 ª

---
+++ Models/LoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial2TareasMVC.Models
+{
+    public class LoginDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Recuérdame")]
+        public bool Recuerdame { get; set; }
+    }
+}
