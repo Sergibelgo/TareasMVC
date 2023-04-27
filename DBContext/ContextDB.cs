d25aa471a1496e8675857f2eaa8d55e1f107f3b8
Sergibelgo <sergibelgo1@gmail.com>
2023-04-27 10:46:38 +0200

    Step 200 finalizado

--- DBContext/ContextDB.cs
+++ DBContext/ContextDB.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Tutorial2TareasMVC.Entitys;
 
 namespace Tutorial2TareasMVC.DBContext
 {
     public interface IContextDB
     {
-
+        DbSet<Tarea> Tareas { get; set; }
     }
     public class ContextDB : DbContext, IContextDB
     {
         public ContextDB(DbContextOptions options) : base(options)
         {
         }
+        public DbSet<Tarea> Tareas { get; set; }
     }
 }
---
+++ Entitys/Tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace Tutorial2TareasMVC.Entitys
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int Orden { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
