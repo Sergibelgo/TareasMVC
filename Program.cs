2d2e17ae7e0db9f7e2fe01026e90beeb08b22da6
Sergibelgo <sergibelgo1@gmail.com>
2023-04-27 13:43:54 +0200

    Step 207 finalizado

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Tutorial2TareasMVC.DBContext;
 
 namespace Tutorial2TareasMVC
@@ -9,11 +13,28 @@ namespace Tutorial2TareasMVC
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddDbContext<ContextDB>(opciones=>opciones.UseSqlServer("name=DefaultConnection"));
+            builder.Services.AddDbContext<ContextDB>(opciones => opciones.UseSqlServer("name=DefaultConnection"));
+            //builder.Services.AddDbContext<ContextDB>(opciones =>
+            //opciones.UseMySql(
+            //    builder.Configuration.GetConnectionString("MariaDbConnectionString"),
+            //    new MariaDbServerVersion(new Version(10, 3, 27))
+            //    ));
+                
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddTransient<IContextDB, ContextDB>();
-
+            builder.Services.AddAuthentication();
+            //Añadir identity
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(opciones =>
+            {
+                opciones.SignIn.RequireConfirmedAccount = false;
+            }).AddEntityFrameworkStores<ContextDB>()
+            .AddDefaultTokenProviders();
+            builder.Services.PostConfigure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme, opciones =>
+            {
+                opciones.LoginPath = "/usuarios/login";
+                opciones.AccessDeniedPath = "/usuarios/login";
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -29,6 +50,7 @@ namespace Tutorial2TareasMVC
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
