38b000f850fcf3cfd7faaa03e8318c0b11c408a5
Sergibelgo <sergibelgo1@gmail.com>
2023-04-27 10:30:59 +0200

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tutorial2TareasMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
