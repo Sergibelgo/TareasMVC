03bd69bd009f8b203d06025eeed3246d1865f84e
Sergibelgo <sergibelgo1@gmail.com>
2023-05-03 13:30:13 +0200

    Step 239 finalizado

---
+++ Controllers/TareasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tutorial2TareasMVC.DBContext;
+using Tutorial2TareasMVC.Services;
+
+namespace Tutorial2TareasMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        private readonly ContextDB _contextDB;
+        private readonly IUserService userService;
+
+        public TareasController(ContextDB contextDB,IUserService userService)
+        {
+            this._contextDB = contextDB;
+            this.userService = userService;
+        }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+
+namespace Tutorial2TareasMVC.Services
+{
+    public interface IUserService
+    {
+
+    }
+    public class UserService:IUserService
+    {
+        private HttpContext context;
+
+        public UserService(IHttpContextAccessor httpContext)
+        {
+            context = httpContext.HttpContext;
+        }
+        public string ObtenerUsuarioId()
+        {
+            if (context.User.Identity.IsAuthenticated)
+            {
+                var idClaim=context.User.Claims
+                    .Where(x=>x.Type==ClaimTypes.NameIdentifier).FirstOrDefault();
+                return idClaim.Value;
+            }
+            else
+            {
+                throw new Exception("El usuario no esta autenticado");
+            }
+        }
+    }
+}
