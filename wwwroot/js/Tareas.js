8e7826a51caf2038174a0684aa31c66fae437058
Sergibelgo <sergibelgo1@gmail.com>
2023-05-04 09:21:00 +0200

    Step 247 finalizado

--- wwwroot/js/Tareas.js
+++ wwwroot/js/Tareas.js
@@ -37,3 +37,29 @@ async function obtenerTareas() {
         tareaListadoViewModel.cargando(false);
     }
 }
+async function actualizarOrdenTareas() {
+    const ids = obtenerIdsTareas();
+    await enviarIdsTareasAlBackend(ids);
+
+    const arregloOrdenado = tareaListadoViewModel.tareas.sorted((a, b) => {
+        return ids.indexOf(a.id().toString()) - ids.indexOf(b.id().toString());
+    })
+    tareaListadoViewModel.tareas([]);
+    tareaListadoViewModel.tareas(arregloOrdenado);
+}
+function obtenerIdsTareas() {
+    const ids = $("[name=titulo-tarea").map(function () {
+        return $(this).attr("data-id");
+    }).get();
+    return ids;
+}
+async function enviarIdsTareasAlBackend(ids) {
+    var data = JSON.stringify(ids);
+    await fetch(`${urlTareas}/ordenar`, {
+        method: "POST",
+        body: data,
+        headers: {
+            "Content-Type":"application/json"
+        }
+    })
+}
