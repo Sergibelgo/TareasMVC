3102003a6187c8d217717ba0a0d97caaaf5e1cba
Sergibelgo <sergibelgo1@gmail.com>
2023-05-04 12:24:40 +0200

    Step 258 finalizado

--- Controllers/TareasController.cs
+++ Controllers/TareasController.cs
@@ -81,7 +81,7 @@ namespace Tutorial2TareasMVC.Controllers
         public async Task<IActionResult> GetTarea(int id)
         {
             var usuarioId = userService.ObtenerUsuarioId();
-            var tarea = await _contextDB.Tareas.Where(t => t.UsuarioCreacion.Id == usuarioId && t.Id==id).FirstOrDefaultAsync();
+            var tarea = await _contextDB.Tareas.Where(t => t.UsuarioCreacion.Id == usuarioId && t.Id==id).Include(t=>t.Pasos).FirstOrDefaultAsync();
             if (tarea is null)
             {
                 return Forbid();
